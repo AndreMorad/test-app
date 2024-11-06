7e82f98e9cccffe273ba09be5f427077d52e81b0
AndreMorad <andre.morad@gmail.com>
2024-11-06 12:53:52 +0100

    ninos

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> this is a test app test
-        </p>
+        <p>Ninos test 1234</p>
         <a
           className="App-link"
           href="https://reactjs.org"
